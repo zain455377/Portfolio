9c92d2345874b29d8b5f46fb2c85140244edaba6
zain455377 <zainjamil455@gmail.com>
2025-08-15 18:51:49 +0500

    Updated CV

--- src/components/ResumeButton.js
+++ src/components/ResumeButton.js
@@ -5,7 +5,7 @@ const ResumeButton = () => {
   return (
     <div className="text-center bg-[#1e293b] dark:bg-gray-800 py-6">
       <a
-        href="/Zain-UL-Abedin-Khan.pdf"
+        href={`${process.env.PUBLIC_URL}/Zain-UL-Abedin-Khan.pdf`}
         download
         className="inline-flex items-center gap-2 bg-pink-600 hover:bg-pink-700 text-white px-6 py-3 rounded-xl text-lg font-semibold transition duration-300 shadow-md"
       >
